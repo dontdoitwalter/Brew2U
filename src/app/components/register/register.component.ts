a8c3f6e3cf0170e441e6e8c72d3f69798c9be1a1
Sam Shaw <sam.shaw.itpro@gmail.com>
2018-12-05 13:34:09 -0500

    before I break login

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, FormControl } from '@angular/forms'
+import { UserService } from 'src/app/services/user/user.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +10,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+ 
+createUser = [];
+users = [];
 
-  constructor() { }
 
-  ngOnInit() {
+  email: string;
+  username: string;
+  password: string;
+  firstName: string;
+  lastName: string;
+  userAddress: string;
+  userSecondAddress: string;
+  userCity: string;
+  userState: string;
+  userZipcode: number;
+  phoneNumber: string;
+  isAdmin: boolean;
+
+
+  constructor(private router: Router, private userservice: UserService, private http: HttpClient) { }
+
+  // ngOnInit() {}
+
+    ngOnInit(): void {
+         this.http.get('http://localhost:3000/info/1').subscribe(data => {
+        console.log(data);
+    })
+  };
+
+
+  register() : void {
+  console.log(this.email, this.phoneNumber);
   }
-
+  
 }
--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders }from '@angular/common/http';
+import { User } from '../../models/userModel';
+import { Observable } from "rxjs";
+
+const httpOptions = {
+  headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+  })
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  private _dbUrl = "http://localhost:3000"
+
+  constructor(private _http: HttpClient) { }
+
+  getUser() : Observable<User[]> {
+    return this._http.get<User[]>(`${this._dbUrl}/all`)
+  }
+  
+  makeUser(user: User) : Observable<User[]> {
+    return this._http.post<User[]>(`${this._dbUrl}/create`, user, httpOptions)
+  }
 }
