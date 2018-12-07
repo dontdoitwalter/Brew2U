09c96048483687ce98f7332adfad02257eec9542
Sam Shaw <sam.shaw.itpro@gmail.com>
2018-12-06 19:19:45 -0500

    can register but token null

--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders }from '@angular/common/http';
 import { User } from '../../models/userModel';
-import { Observable } from "rxjs";
+import { BehaviorSubject, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 const httpOptions = {
   headers: new HttpHeaders({
@@ -13,20 +14,48 @@ const httpOptions = {
   providedIn: 'root'
 })
 export class UserService {
+  private currentUserSubject: BehaviorSubject<User>;
+  public currentUser: Observable<User>;
 
-    private _dbUrl = "http://localhost:3000"
 
-  constructor(private http: HttpClient) { }
 
-//   getById(id: number) {
-//     return this.http.get<User>(`${apiUrl}/users/${id}`);
-// }
+  email: string;
+  username: string;
+  password: string;
+  firstName: string;
+  lastName: string;
+  userAddress: string;
+  userSecondAddress: string;
+  userCity: string;
+  userState: string;
+  userZipcode: number;
+  phoneNumber: string;
+  // isAdmin: boolean;
+  isAdmin: boolean = false
 
-//   getUser() : Observable<User[]> {
-//     return this._http.get<User[]>(`${this._dbUrl}/all`)
-//   }
-  
-//   makeUser(user: User) : Observable<User[]> {
-//     return this._http.post<User[]>(`${this._dbUrl}/create`, user, httpOptions)
-//   }
+   
+  // constructor(private http: HttpClient) { }
+
+  constructor(private http: HttpClient) {
+    this.currentUserSubject = new BehaviorSubject<User>(JSON.parse(localStorage.getItem('currentUser')));
+    this.currentUser = this.currentUserSubject.asObservable();
+}
+
+public get currentUserValue(): User {
+  return this.currentUserSubject.value;
+}
+
+  register(email, username, password, firstName, lastName, userAddress, userSecondAddress, userCity, userState ,userZipcode, phoneNumber, isAdmin) {
+    return this.http.post<any>(`http://localhost:3000/user/signup`, {user: {email, username, password, firstName, lastName, userAddress, userSecondAddress, userCity, userState ,userZipcode, phoneNumber, isAdmin }})
+    .pipe(map(user => {
+      // SAM /////////////////// fix this
+      if (user && user) {
+          
+          localStorage.setItem('token', user.sessiontoken);
+ 
+      }
+
+      return user;
+  }));
+}
 }
