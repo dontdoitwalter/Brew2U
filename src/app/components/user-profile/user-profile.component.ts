cdd50a882f4593767dc9de9071c45527e914eb4d
dontdoitwalter <emailwaltervanhooser@gmail.com>
2018-12-14 13:12:41 -0500

    first commit

--- src/app/components/user-profile/user-profile.component.ts
+++ src/app/components/user-profile/user-profile.component.ts
@@ -29,38 +29,8 @@ export class UserProfileComponent implements OnInit {
 
   ngOnInit() {
 
-    //this.order = this.orderservice.getOrders();
+    // this.order = this.orderservice.getOrders();
   
 
   }
 }
-  
-//   this.service.getProducts().subscribe(
-  //     list => {
-  //       let array = list.map(item => {
-  //         let userName = this.userservice.getUserName(item.payload.val()['user']); //need a username function 
-  //         return {
-  //           $key: item.key,
-  //           userName,
-  //           ...item.payload.val()
-  //         };
-  //       });
-  //       this.listData = new MatTableDataSource(array);
-  //       this.listData.sort = this.sort;
-  //       this.listData.paginator = this.paginator;
-  //       this.listData.filterPredicate = (data, filter) => {
-  //         return this.displayedColumns.some(ele => {
-  //           return ele != 'actions' && data[ele].toLowerCase().indexOf(filter) != -1;
-  //         });
-  //       };
-  //     });
-  // }
-
-  // onSearchClear() {
-  //   this.searchKey = "";
-  //   this.applyFilter();
-  // }
-
-  // applyFilter() {
-  //   this.listData.filter = this.searchKey.trim().toLowerCase();
-
